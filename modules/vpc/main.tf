18b713068cd49ec1fba91133e3baae10b71ef39d
silentknight2001 <silentknight.www@gmail.com>
2026-03-05 11:22:10 +0530

    feat: add modules/vpc/main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public" {
+    count = 2
+    vpc_id = aws_vpc.this.id
+    cidr_block = var.public_subnets[count.index]
+    availability_zone = "${var.region}${count.index == 0 ? "a" : "b"}"
+    map_public_ip_on_launch = true 
+}
+
+resource "aws_subnet" "private" {
+    count = 2
+    vpc_id = aws_vpc.this
+    cidr_block = var.private_subnets[count.index]
+    availability_zone = "${var.region}${count.index == 0 ? "a" : "b"}"
+}
