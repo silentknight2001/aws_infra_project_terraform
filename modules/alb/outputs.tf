24f9398c5144a1a32668bea7e5dbe52dc78a55d8
silentknight2001 <silentknight.www@gmail.com>
2026-03-06 12:11:11 +0530

    feat: add modules/alb/outputs.tf

---
+++ modules/alb/outputs.tf
@@ -0,0 +1,9 @@
+output "alb_dns" {
+    value = aws_alb.this.dns_name
+}
+output "target_group_arn" {
+  value = aws_lb_target_group.tg.arn
+}
+output "alb_sg_id" {
+  value = aws_security_group.alb_sg.id 
+}
