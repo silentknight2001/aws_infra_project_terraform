c96de4107b9c78e7e2e4cf3a42185c0f4386b2ee
silentknight2001 <silentknight.www@gmail.com>
2026-03-14 20:53:54 +0530

    fix: root variable.tf typo (string)

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "region" {}
-variable "vpc_cidr" {}
-variable "public_subnets" {type = list(stirng)}
-variable "private_subnets" {type = list(stirng)}
-variable "instance_type" {}
+variable "region" {type = string}
+variable "vpc_cidr" {type = string}
+variable "public_subnets" {type = string}
+variable "private_subnets" {type = string}
+variable "instance_type" {type = string}
 variable "ami_id" {}
 variable "db_username" {}
 variable "db_password" { sensitive = true }
