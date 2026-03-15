546abea20026e155497f59c637f09cf0159e51d8
silentknight2001 <silentknight.www@gmail.com>
2026-03-15 21:35:37 +0530

    fix: typo list

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" { type = string }
 variable "vpc_cidr" { type = string }
-variable "public_subnets" { type = string }
-variable "private_subnets" { type = string }
+variable "public_subnets" { type = list(string) }
+variable "private_subnets" { type = list(string) }
 variable "instance_type" { type = string }
 variable "ami_id" {}
 variable "db_username" {}
