b1fd7641a1d723ee7bb24fc4d1fec58402556cd1
silentknight2001 <silentknight.www@gmail.com>
2026-03-16 21:07:01 +0530

    fix: chnage asg public to private

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -2,7 +2,7 @@ resource "aws_autoscaling_group" "asg" {
     desired_capacity = 2
     max_size = 3
     min_size = 2
-    vpc_zone_identifier = var.public_subnets
+    vpc_zone_identifier = var.private_subnets
 
     launch_template {
       id = var.launch_template_id
