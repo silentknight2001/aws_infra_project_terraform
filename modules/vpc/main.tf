5ee0b182afa4df83edb8eb209a3eac68ddd853b5
silentknight2001 <silentknight.www@gmail.com>
2026-03-16 11:38:37 +0530

    fix: corection of vpc/main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,19 +1,14 @@
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
+   tags = {
+    Name = "tf-project-vpc"
+  }
 }
 
-resource "aws_subnet" "public" {
-    count = 2
-    vpc_id = aws_vpc.this.id
-    cidr_block = var.public_subnets[count.index]
-    availability_zone = "${var.region}${count.index == 0 ? "a" : "b"}"
-    map_public_ip_on_launch = true 
-}
-
-resource "aws_subnet" "private" {
-    count = 2
-    vpc_id = aws_vpc.this.id
-    cidr_block = var.private_subnets[count.index]
-    availability_zone = "${var.region}${count.index == 0 ? "a" : "b"}"
+resource "aws_internet_gateway" "tf_igw" {
+    vpc_id = aws_vpc.this
+    tags = {
+      Name = "tf-igw"
+    }
 }
