c88641c4476fb801f6088bacbfb1c0192d6a9025
silentknight2001 <silentknight.www@gmail.com>
2026-03-18 19:30:13 +0530

    modify: rds/main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,16 +1,42 @@
-resource "aws_db_subnet_group" "db_subnet" {
-    name = "tf_test_db_subnet"
-    subnet_ids = var.private_subnets
+resource "aws_db_subnet_group" "tf_db_subnet" {
+    name = "tf_test_db_subnet_group"
+    subnet_ids = var.private_subnet_ids
 }
 
-resource "aws_db_instance" "mysql" {
+resource "aws_security_group" "rds_sg" {
+  name   = "rds-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    security_groups = [var.app_sg_id]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "primary" {
+    identifier = "primary-db"
+
     allocated_storage = 20
     engine = "mysql"
     instance_class = "db.t3.micro"
+
     db_name = "tf_test_mysql"
     username = var.db_username
     password = var.db_password
     multi_az = true
-    db_subnet_group_name = aws_db_subnet_group.db_subnet.name
+    publicly_accessible = false
+
+    db_subnet_group_name = aws_db_subnet_group.tf_db_subnet.name
+    vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+    backup_retention_period = 7
     skip_final_snapshot = true
 }
