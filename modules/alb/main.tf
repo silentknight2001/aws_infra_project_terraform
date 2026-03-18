59799de8be89b6abc2466f9f68e7543711b97405
silentknight2001 <silentknight.www@gmail.com>
2026-03-18 20:12:26 +0530

    tag: add tag in sg

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -13,6 +13,9 @@ resource "aws_security_group" "alb_sg" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "alb-security-group"
+  }
 }
 resource "aws_alb" "this" {
     name = "tf-test-alb"
