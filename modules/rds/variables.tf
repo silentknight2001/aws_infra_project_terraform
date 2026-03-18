4fbf98abf68ec90d7f36334eb2e8d460b1ce581b
silentknight2001 <silentknight.www@gmail.com>
2026-03-18 20:38:07 +0530

    ft: add variables

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,4 +1,5 @@
-variable "private_subnets" { type = list(string)}
 variable "vpc_id" {}
+variable "private_subnet_ids" { type = list(string)}
+variable "app_sg_id" {}
 variable "db_username" {}
 variable "db_password" {}
