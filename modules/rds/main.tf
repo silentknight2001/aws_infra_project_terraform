390191894979dc00fb9d64ca61795c2b480aa635
silentknight2001 <silentknight.www@gmail.com>
2026-03-29 22:35:09 +0530

    fix: error resolve

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = db-security-groups
+    Name = "db-security-groups"
   }
 }
 
@@ -34,7 +34,7 @@ resource "aws_db_instance" "primary" {
 
     allocated_storage = 20
     engine = "mysql"
-    engine_version = 8.0
+    engine_version = "8.0"
     instance_class = "db.t3.micro"
 
     db_name = "tf_test_mysql"
@@ -53,4 +53,12 @@ resource "aws_db_instance" "primary" {
     tags = {
       Name = "primary-rds-db"
     }
+}
+
+resource "aws_db_instance" "replica" {
+  provider = aws.virginia
+
+  replicate_source_db = aws_db_instance.primary.arn
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
 }
