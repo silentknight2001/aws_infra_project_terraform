08079cba1a346d163df1e6df5902facb54f523c6
silentknight2001 <silentknight.www@gmail.com>
2026-03-29 22:48:15 +0530

    fix: typo ID

--- modules/vpc/route_tables.tf
+++ modules/vpc/route_tables.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "tf_rt_public" {
   vpc_id = aws_vpc.this.id
 }
 resource "aws_route" "public_internet" {
-    route_table_id = aws_route_table.pubic.id
+    route_table_id = aws_route_table.tf_rt_public.id
     destination_cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.tf_igw.id 
 }
@@ -19,7 +19,7 @@ resource "aws_route_table" "tf_rt_private" {
 }
 
 resource "aws_route" "privet_nat_route" {
-  route_table_id = aws_route_table.private.id 
+  route_table_id = aws_route_table.tf_rt_private.id 
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.tf_nat.id 
 }
@@ -28,5 +28,5 @@ resource "aws_route_table_association" "privet_nat_route_associa" {
   count = length(aws_subnet.private)
 
   subnet_id = aws_subnet.private[count.index].id 
-  route_table_id = aws_route_table.tf_rt_private
+  route_table_id = aws_route_table.tf_rt_private.id
 }
