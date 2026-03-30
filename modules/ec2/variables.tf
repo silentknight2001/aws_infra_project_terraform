6633174976ca8f2f50b2e0dca0f6fd7c35ffe843
silentknight2001 <silentknight.www@gmail.com>
2026-03-30 22:23:07 +0530

    fix: add new vaiables

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,3 +2,9 @@ variable "ami_id" {}
 variable "instance_type" {}
 variable "vpc_id" {}
 variable "alb_sg_id" {}
+variable "db_endpoint" {
+  description = "RDS endpoint"
+  type        = string
+}
+variable "db_username" {}
+variable "db_password" {}
