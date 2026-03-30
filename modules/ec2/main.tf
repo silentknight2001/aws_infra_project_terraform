d9602fa7da2dd2f7af8096c4cca5f11b231799b6
silentknight2001 <silentknight.www@gmail.com>
2026-03-30 22:21:40 +0530

    fix: php page and db connection configured

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,16 +1,16 @@
 resource "aws_security_group" "app_sg" {
   vpc_id = var.vpc_id
-  
+
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = [var.alb_sg_id]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
@@ -18,19 +18,32 @@ resource "aws_security_group" "app_sg" {
   }
 }
 resource "aws_launch_template" "lt" {
-  name_prefix = "tf_test_template"
-  image_id = var.ami_id
+  name_prefix   = "tf_test_template"
+  image_id      = var.ami_id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.app_sg.id]
-  
+
   user_data = base64encode(<<EOF
-  #!/bin/bash
-  apt update 
-  apt install httpd php -y
-  systemctl start httpd
-  systemctl enable httpd
-  echo "Hello from ASG instance" > /var/www/html/index.html
-  EOF
+#!/bin/bash
+apt update -y
+apt install apache2 php php-pgsql -y
+
+systemctl start apache2
+systemctl enable apache2
+
+cat <<EOT > /var/www/html/index.php
+<?php
+\$conn = pg_connect("host=${var.db_endpoint} dbname=postgres user={var.db_username} password=(var.db_password)");
+
+if (!\$conn) {
+    die("Connection failed");
+}
+
+echo "Connected successfully";
+?>
+EOT
+
+EOF
   )
 }
