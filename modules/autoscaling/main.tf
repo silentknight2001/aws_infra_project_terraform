a0723b2a6b66a163ebca67ca3d37bf883016eaa8
silentknight2001 <silentknight.www@gmail.com>
2026-03-30 22:16:38 +0530

    fomet doc

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -1,14 +1,14 @@
 resource "aws_autoscaling_group" "asg" {
-    desired_capacity = 2
-    max_size = 3
-    min_size = 2
-    vpc_zone_identifier = var.private_subnets
-    
-    health_check_type = "ELB"
-    
-    launch_template {
-      id = var.launch_template_id
-      version = "$Latest"
-    }
-    target_group_arns = [var.target_group_arn]
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 2
+  vpc_zone_identifier = var.private_subnets
+
+  health_check_type = "ELB"
+
+  launch_template {
+    id      = var.launch_template_id
+    version = "$Latest"
+  }
+  target_group_arns = [var.target_group_arn]
 }
