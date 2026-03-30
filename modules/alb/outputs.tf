f1b62bd9304f626fe13272d14a1863157e7f7ac3
silentknight2001 <silentknight.www@gmail.com>
2026-03-30 22:14:35 +0530

    fomet doc

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,9 +1,9 @@
 output "alb_dns" {
-    value = aws_alb.this.dns_name
+  value = aws_alb.this.dns_name
 }
 output "target_group_arn" {
   value = aws_lb_target_group.tg.arn
 }
 output "alb_sg_id" {
-  value = aws_security_group.alb_sg.id 
+  value = aws_security_group.alb_sg.id
 }
