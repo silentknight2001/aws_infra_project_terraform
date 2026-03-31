f8e902fe7d85953d834ff3713a3d4f8da9e4beb5
silentknight2001 <silentknight.www@gmail.com>
2026-03-31 20:14:14 +0530

    error resolve

--- modules/vpc/nat_gateway.tf
+++ modules/vpc/nat_gateway.tf
@@ -3,9 +3,9 @@ resource "aws_eip" "nat_eip" {
 }
 
 resource "aws_nat_gateway" "tf_nat" {
-    allocation_id = aws_eip.nat_eip.id 
-    subnet_id = aws_subnet.public[0].id
-    tags = {
-        Name = "tf-nat"
-    }
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public[0].id
+  tags = {
+    Name = "tf-nat"
+  }
 }
