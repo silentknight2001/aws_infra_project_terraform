81fb1cd1ac9114a94a2cc1f73c0edfceb200af8e
silentknight2001 <silentknight.www@gmail.com>
2026-03-31 20:05:38 +0530

    fix: change db name

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,27 +1,27 @@
 resource "aws_db_subnet_group" "tf_db_subnet" {
-    name = "tf_test_db_subnet_group"
-    subnet_ids = var.private_subnet_ids
+  name       = "tf_test_db_subnet_group"
+  subnet_ids = var.private_subnet_ids
 
-    tags = {
-      Name = "tf-test-db-subnet-group"
-    }
+  tags = {
+    Name = "tf-test-db-subnet-group"
+  }
 }
 
 resource "aws_security_group" "rds_sg" {
-  name   = "rds-sg"
-  description = "Allow MySQL access from application server"
-  vpc_id = var.vpc_id
+  name        = "rds-sg"
+  description = "Allow Postgres access from application server"
+  vpc_id      = var.vpc_id
 
   ingress {
-    from_port = 3306
-    to_port   = 3306
-    protocol  = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
     security_groups = [var.app_sg_id]
   }
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
@@ -30,29 +30,29 @@ resource "aws_security_group" "rds_sg" {
 }
 
 resource "aws_db_instance" "primary" {
-    identifier = "primary-db"
+  identifier = "primary-db"
 
-    allocated_storage = 20
-    engine = "mysql"
-    engine_version = "8.0"
-    instance_class = "db.t3.micro"
+  allocated_storage = 20
+  engine            = "postgres"
+  engine_version    = "15"
+  instance_class    = "db.t3.micro"
 
-    db_name = "tf_test_mysql"
-    username = var.db_username
-    password = var.db_password
+  db_name  = "tf_test_postgres"
+  username = var.db_username
+  password = var.db_password
 
-    multi_az = true
-    publicly_accessible = false
+  multi_az            = true
+  publicly_accessible = false
 
-    db_subnet_group_name = aws_db_subnet_group.tf_db_subnet.name
-    vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.tf_db_subnet.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
-    backup_retention_period = 7
-    skip_final_snapshot = true
+  backup_retention_period = 7
+  skip_final_snapshot     = true
 
-    tags = {
-      Name = "primary-rds-db"
-    }
+  tags = {
+    Name = "primary-rds-db"
+  }
 }
 
 resource "aws_db_instance" "replica" {
