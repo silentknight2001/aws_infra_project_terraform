55d32074ab320d3584baee15862f7b5fd721cd3a
silentknight2001 <silentknight.www@gmail.com>
2026-03-31 20:08:38 +0530

    fix: format doc

--- modules/rds/terraform.tf
+++ modules/rds/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      configuration_aliases = [ aws.virginia ]
+      source                = "hashicorp/aws"
+      configuration_aliases = [aws.virginia]
     }
   }
 }
