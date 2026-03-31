9d844951044c476ae8812a89c28ac162e538d715
silentknight2001 <silentknight.www@gmail.com>
2026-03-31 20:12:47 +0530

    error resolve

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,14 +1,14 @@
 resource "aws_vpc" "this" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-   tags = {
+  tags = {
     Name = "tf-project-vpc"
   }
 }
 
 resource "aws_internet_gateway" "tf_igw" {
-    vpc_id = aws_vpc.this.id
-    tags = {
-      Name = "tf-igw"
-    }
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "tf-igw"
+  }
 }
