79d7f3dd8f4b2891813491a222243b903104e09a
silentknight2001 <silentknight.www@gmail.com>
2026-04-03 10:51:20 +0530

    error reoslve

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -1,16 +1,16 @@
 resource "aws_subnet" "public" {
-    count = 2
+  count = 2
 
-    vpc_id = aws_vpc.this.id
-    cidr_block = var.public_subnets[count.index]
-    availability_zone = "${var.region}${count.index == 0 ? "a" : "b"}"
-    map_public_ip_on_launch = true 
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = "${var.region}${count.index == 0 ? "a" : "b"}"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
-    count = 2
-    
-    vpc_id = aws_vpc.this.id
-    cidr_block = var.private_subnets[count.index]
-    availability_zone = "${var.region}${count.index == 0 ? "a" : "b"}"
+  count = 2
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = "${var.region}${count.index == 0 ? "a" : "b"}"
 }
