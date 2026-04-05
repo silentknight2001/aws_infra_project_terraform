1ade6b845fe25ddb70dde201f6dbaaf277a8fd82
silentknight2001 <silentknight.www@gmail.com>
2026-04-05 23:17:57 +0530

    delete: delete file which not anymore require' '

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-output "alb_dns" {
-  value = module.alb.alb_dns
-}
-output "rds_endpoint" {
-  value = module.rds.db_endpoint
-}
--- s3_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "silentknight-terraform-state-bucket"
-    key = "project/terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt = true
-  }
-}
