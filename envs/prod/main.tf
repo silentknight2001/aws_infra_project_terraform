ab2760b31285986a4e134f6c821fc99bf8fd747b
silentknight2001 <silentknight.www@gmail.com>
2026-04-06 20:23:48 +0530

    rename file s3_backend --> backend

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source          = "./modules/vpc"
+  source          = "../../modules/vpc"
   vpc_cidr        = var.vpc_cidr
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
@@ -7,13 +7,13 @@ module "vpc" {
 }
 
 module "alb" {
-  source         = "./modules/alb"
+  source         = "../../modules/alb"
   vpc_id         = module.vpc.vpc_id
   public_subnets = module.vpc.public_subnet_ids
 }
 
 module "ec2" {
-  source        = "./modules/ec2"
+  source        = "../../modules/ec2"
   db_endpoint   = module.rds.db_endpoint
   db_username   = var.db_username
   db_password   = var.db_password
@@ -24,14 +24,14 @@ module "ec2" {
 }
 
 module "autoscaling" {
-  source             = "./modules/autoscaling"
+  source             = "../../modules/autoscaling"
   launch_template_id = module.ec2.launch_template_id
   private_subnets    = module.vpc.private_subnet_ids
   target_group_arn   = module.alb.target_group_arn
 }
 
 module "rds" {
-  source             = "./modules/rds"
+  source             = "../../modules/rds"
   private_subnet_ids = module.vpc.private_subnet_ids
   vpc_id             = module.vpc.vpc_id
   db_username        = var.db_username
