82f79e71d5204adb7f0034ddb11aeeb77d93639a
silentknight2001 <silentknight.www@gmail.com>
2026-04-10 23:05:15 +0530

    fix

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -5,6 +5,10 @@ resource "aws_subnet" "public" {
   cidr_block              = var.public_subnets[count.index]
   availability_zone       = "${var.region}${count.index == 0 ? "a" : "b"}"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public subnet"
+  }
 }
 
 resource "aws_subnet" "private" {
@@ -13,4 +17,7 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.private_subnets[count.index]
   availability_zone = "${var.region}${count.index == 0 ? "a" : "b"}"
+   tags = {
+    Name = "Privet subnet"
+  }
 }
